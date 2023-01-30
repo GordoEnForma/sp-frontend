33e43af0ddd49eeef30f3860caa869145dc0a9e3
Gustavo <alonsoem9@gmail.com>
2023-01-30 13:09:42 -0500

    Se finalizo el form para añadir usuario, queda pendiente el uso de la API

--- src/admin/components/UserForm.tsx
+++ src/admin/components/UserForm.tsx
@@ -8,159 +8,193 @@ import {
     Select,
     TextField,
 } from "@mui/material";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { grey } from "@mui/material/colors";
 
+type NameType =
+    | "nombres"
+    | "apellidos"
+    | "correo"
+    | "contraseña"
+    | "state"
+    | "product";
+
+type ValueSchema = {
+    value: string;
+    title: string;
+};
+
+type InputsSchema = {
+    labelId: string;
+    labelTitle: string;
+    type?: string;
+    name: NameType;
+    values?: ValueSchema[];
+};
+const textInputFields: InputsSchema[] = [
+    {
+        labelId: "text-name-id",
+        labelTitle: "Nombres",
+        type: "text",
+        name: "nombres",
+    },
+    {
+        labelId: "text-lastname-id",
+        labelTitle: "Apellidos",
+        type: "text",
+        name: "apellidos",
+    },
+    {
+        labelId: "text-email-id",
+        labelTitle: "Correo",
+        type: "email",
+        name: "correo",
+    },
+    {
+        labelId: "text-password-id",
+        labelTitle: "Contraseña",
+        type: "text",
+        name: "contraseña",
+    },
+];
+
+const selectInputFields: InputsSchema[] = [
+    {
+        labelId: "user-products",
+        labelTitle: "Productos",
+        name: "product",
+        values: [
+            { value: "oficial", title: "Oficial" },
+            { value: "suboficial", title: "Suboficial" },
+        ],
+    },
+    {
+        labelId: "user-state",
+        labelTitle: "Estado",
+        name: "state",
+        values: [
+            { value: "inactivo", title: "Inactivo" },
+            { value: "pendiente", title: "Pendiente" },
+            { value: "activo", title: "Activo" },
+        ],
+    },
+];
+
+interface DataSchema {
+    nombres: string;
+    apellidos: string;
+    correo: string;
+    contraseña: string;
+    state: string;
+    product: string;
+}
 export const UserForm = () => {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        register,
+        watch,
+    } = useForm({
+        defaultValues: {
+            nombres: "",
+            apellidos: "",
+            correo: "",
+            contraseña: "",
+            state: "activo",
+            product: "oficial",
+        },
+    });
+
+    const onSubmit: SubmitHandler<DataSchema> = (data) => {
+        console.log(data);
+    };
+
     return (
         <Grid
             item
             xs={12}
-            md={7}
+            md={7.5}
             sx={{
-                mt: 5,
+                mt: 2,
+                padding: 1,
                 bgcolor: grey[300],
             }}
         >
-            <Box component="form" autoComplete="off" noValidate>
-                <Grid container direction={"row"}>
-                    <Grid
-                        item
-                        xs={12}
-                        lg={5.5}
-                        sx={{
-                            ml: 2,
-                            mt: 2,
-                        }}
-                    >
-                        <TextField
-                            fullWidth
-                            label="Nombres "
-                            variant={"outlined"}
-                            size="medium"
-                            autoComplete="off"
-                        />
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        lg={5.5}
-                        sx={{
-                            ml: 2,
-                            mt: 2,
-                        }}
-                    >
-                        <TextField
-                            fullWidth
-                            label="Apellidos"
-                            variant={"outlined"}
-                            size="medium"
-                            autoComplete="off"
-                        />
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        lg={5.5}
-                        sx={{
-                            ml: 2,
-                            mt: 2,
-                        }}
-                    >
-                        <TextField
-                            sx={{}}
-                            type={"email"}
-                            fullWidth
-                            label="Correo"
-                            variant={"outlined"}
-                            size="medium"
-                            autoComplete="off"
-                        />
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        lg={5.5}
-                        sx={{
-                            ml: 2,
-                            mt: 2,
-                        }}
-                    >
-                        <TextField
-                            fullWidth
-                            label="Contraseña"
-                            variant={"outlined"}
-                            size="medium"
-                            autoComplete="off"
-                        />
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        lg={5.5}
-                        sx={{
-                            ml: 2,
-                            my: 2,
-                        }}
-                    >
-                        <FormControl
-                            sx={{
-                                width: "100%",
-                            }}
-                        >
-                            <InputLabel id="products">Producto</InputLabel>
-                            <Select
-                                labelId="products"
-                                id="demo-simple-select"
-                                // value={age}
-                                label="products"
-                                // onChange={handleChange}
-                            >
-                                <MenuItem value={"suboficial"}>
-                                    SubOficial
-                                </MenuItem>
-                                <MenuItem value={"oficial"}>Oficial</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        lg={5.5}
-                        sx={{
-                            ml: 2,
-                            my: 2,
-                        }}
-                    >
-                        <FormControl
-                            sx={{
-                                width: "100%",
-                            }}
-                        >
-                            <InputLabel id="user-state">Estado</InputLabel>
-                            <Select
-                                labelId="user-state"
-                                id="demo-simple-select"
-                                // value={age}
-                                label="user-state"
-                                // onChange={handleChange}
-                            >
-                                <MenuItem value={"suboficial"}>
-                                    Inactivo
-                                </MenuItem>
-                                <MenuItem value={"pendiente"}>
-                                    Pendiente
-                                </MenuItem>
-                                <MenuItem value={"pendiente"}>Activo</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        justifyContent="center"
-                        display={"flex"}
-                        sx={{ m: 2 }}
-                    >
+            <Box
+                component="form"
+                autoComplete="off"
+                noValidate
+                onSubmit={handleSubmit(onSubmit)}
+                
+            >
+                <Grid container direction={"row"} gap={2}>
+                    {textInputFields.map(
+                        ({ labelTitle, name, type }: InputsSchema, index) => (
+                            <Controller
+                                key={index}
+                                name={name}
+                                control={control}
+                                render={({ field }) => (
+                                    <Grid item xs={12} lg={5.5}>
+                                        <TextField
+                                            sx={{
+                                                fontWeight: 500
+                                            }}
+                                            fullWidth
+                                            variant={"outlined"}
+                                            size="medium"
+                                            autoComplete="off"
+                                            type={type}
+                                            label={labelTitle}
+                                            {...field}
+                                        />
+                                    </Grid>
+                                )}
+                            />
+                        )
+                    )}
+
+                    {selectInputFields.map(
+                        ({ labelTitle, values, name, labelId }, index) => (
+                            <Controller
+                                key={index}
+                                name={name}
+                                control={control}
+                                render={({ field }) => (
+                                    <Grid item xs={12} lg={5.5} sx={{}}>
+                                        <FormControl
+                                            sx={{
+                                                width: "100%",
+                                            }}
+                                        >
+                                            <InputLabel id="products">
+                                                {labelTitle}
+                                            </InputLabel>
+                                            <Select
+                                                {...field}
+                                                labelId={labelId}
+                                                // id="products"
+                                                value={watch(name)}
+                                            >
+                                                {values?.map(
+                                                    ({ title, value }) => (
+                                                        <MenuItem
+                                                            key={value}
+                                                            value={value}
+                                                        >
+                                                            {title}
+                                                        </MenuItem>
+                                                    )
+                                                )}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                )}
+                            />
+                        )
+                    )}
+                    <Grid item xs={12} justifyContent="center" display={"flex"}>
                         <Button
                             type="submit"
                             sx={{ width: "30%" }}
@@ -170,6 +204,7 @@ export const UserForm = () => {
                         </Button>
                     </Grid>
                 </Grid>
+                {/* <pre>{JSON.stringify(watch(), null, 2)}</pre> */}
             </Box>
         </Grid>
     );
